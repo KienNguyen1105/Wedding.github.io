dacd0a4524cdf7400476b97175f4f9da76023cf5
VanKien <nguyenvankien110597@gmail.com>
2023-11-11 13:55:12 +0700

    add

--- js/main.js
+++ js/main.js
@@ -52,7 +52,7 @@
     const cardElement = document.getElementById('cardElement');
     if (cardElement) {
         cardElement.addEventListener('mouseup', function () {
-            playAudio(true);
+            // playAudio(true);
             flipCard(this);
             setTimeout(function() {
                 moveImageUp();
@@ -163,7 +163,7 @@
     function preloader() {
         if ($('.preloader').length) {
             if(app && content)
-                app.removeChild(content);
+                // app.removeChild(content);
             $('.preloader').delay(200).fadeOut(500, function () {
                 if(app && preloadercls)
                     app.removeChild(preloadercls);
@@ -171,33 +171,6 @@
         }
     }
 
-//     /*================================
-//      sticky-header
-//      ==================================*/
-//     $(window).scroll(function () {
-
-//         if ($(window).scrollTop() > 10) {
-//             $('.sticky-header').addClass('sticky'),
-//                 $('.scrollup').addClass('show_hide');
-//         } else {
-//             $('.sticky-header').removeClass('sticky'),
-//                 $('.scrollup').removeClass('show_hide');
-//         }
-
-//     });
-
-
-//     /*================================
-//         slicknav
-//         ==================================*/
-
-//     $('#nav_mobile_menu').slicknav({
-//         label: '',
-//         duration: 1000,
-//         easingOpen: "easeOutBounce", //available with jQuery UI
-//         prependTo: '#mobile_menu'
-//     });
-    
     /*================================
     slider-area content effect
     ==================================*/
@@ -205,53 +178,6 @@
         $('.slider-two').addClass('scontent_loaded');
         $('.slider-parallax').addClass('scontent_loaded');
     }
-
-
-//     /*================================
-//       Isotope Portfolio
-//      ==================================*/
-//     $('.grid').imagesLoaded(function () {
-
-//         // filter items on button click
-//         $('.gallery-menu').on('click', 'button', function () {
-//             var filterValue = $(this).attr('data-filter');
-//             $grid.isotope({
-//                 filter: filterValue
-//             });
-//         });
-
-//         // init Isotope
-//         var $grid = $('.grid').isotope({
-//             itemSelector: '.grid-item',
-//             percentPosition: true,
-//             masonry: {
-//                 // use outer width of grid-sizer for columnWidth
-//                 columnWidth: '.grid-item',
-//             }
-//         });
-
-
-
-//     });
-
-//     $('.gallery-menu button').on('click', function () {
-//         $('.gallery-menu button').removeClass('active');
-//         $(this).addClass('active');
-//     });
-
-
-//     /*------------------------------------------
-//         = COUNTDOWN CLOCK
-//     -------------------------------------------*/
-//     if ($("#clock").length) {
-//         $('#clock').countdown('2023/05/13', function (event) {
-//             var $this = $(this).html(event.strftime('' +
-//                 '<div class="box"><div class="date">%D</div> <span>Days</span> </div>' +
-//                 '<div class="box"><div class="date">%H</div> <span>Hours</span> </div>' +
-//                 '<div class="box"><div class="date">%M</div> <span>Mins</span> </div>' +
-//                 '<div class="box"><div class="date">%S</div> <span>Secs</span> </div>'));
-//         });
-//     }
 
     /*================================
      Variable Initialize
@@ -278,117 +204,6 @@
             return false;
         });
     }
-
-//     /*------------------------------------------
-//         = BACK TO TOP
-//     -------------------------------------------*/
-//     if ($(".scrollup").length) {
-//         $(".scrollup").on("click", function () {
-//             $("html,body").animate({
-//                 scrollTop: 0
-//             }, 1000, "easeInOutExpo");
-//             return false;
-//         })
-//     }
-
-
-//     /*================================
-//     Magnific Popup
-//     ==================================*/
-//     if ($(".expand-img").length) {
-//         $('.expand-img').magnificPopup({
-//             type: 'image',
-//             gallery: {
-//                 enabled: true
-//             }
-//         });
-
-//         $('.expand-video').magnificPopup({
-//             type: 'iframe',
-//             gallery: {
-//                 enabled: true
-//             }
-//         });
-//     }
-//     /*------------------------------------------
-//         = WATER RIPPLE
-//     -------------------------------------------*/
-//     if ($(".ripple").length) {
-//         $('.ripple').ripples({
-//             resolution: 512,
-//             dropRadius: 20, //px
-//             perturbance: 0.04,
-//         });
-
-//         // Automatic drops
-//         setInterval(function () {
-//             var $el = $('.ripple');
-//             var x = Math.random() * $el.outerWidth();
-//             var y = Math.random() * $el.outerHeight();
-//             var dropRadius = 20;
-//             var strength = 0.04 + Math.random() * 0.04;
-
-//             $el.ripples('drop', x, y, dropRadius, strength);
-//         }, 400);
-//     }
-
-//     if ($(".particleground").length) {
-//         $('.particleground').particleground({
-//             dotColor: '#999999',
-//             lineColor: '#999999',
-//             particleRadius: 5,
-//             lineWidth: 2,
-//             curvedLines: true,
-//             proximity: 20,
-//             parallaxMultiplier: 10,
-//         });
-
-//     }
-
-
-//     /*------------------------------------------
-//         = VIDEO BACKGROUND
-//     -------------------------------------------*/
-//     if ($("#video-background").length) {
-//         $('#video-background').YTPlayer({
-//             showControls: false,
-//             playerVars: {
-//                 modestbranding: 0,
-//                 autoplay: 1,
-//                 controls: 1,
-//                 showinfo: 0,
-//                 wmode: 'transparent',
-//                 branding: 0,
-//                 rel: 0,
-//                 autohide: 0,
-//                 origin: window.location.origin
-//             }
-//         });
-//     }
-
-//     /*------------------------------------------
-//         = POPUP YOUTUBE, VIMEO, GMAPS
-//     -------------------------------------------*/
-//     $('.popup-youtube, .popup-vimeo, .popup-gmaps').magnificPopup({
-//         type: 'iframe',
-//         mainClass: 'mfp-fade',
-//         removalDelay: 160,
-//         preloader: false,
-//         fixedContentPos: false
-//     });
-
-
-//     /*------------------------------------------
-//         = TOGGLE MUSUC BIX
-//     -------------------------------------------*/
-//     if ($(".music-box").length) {
-//         var musicBtn = $(".music-box-toggle-btn");
-
-//         musicBtn.on("click", function () {
-//             playAudio();
-//             return false;
-//         })
-//     }
 
     function playAudio(play) {
         if (audio.paused) {
