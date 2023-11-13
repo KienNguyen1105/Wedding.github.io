b544aacbe8960c388ce1ea4d0df5440a27d9347d
KienNV <kiennv@huongvietgroup.com>
2023-11-13 12:23:55 +0700

    asss

--- js/main.js
+++ js/main.js
@@ -260,6 +260,8 @@
 
     var sync1 = $("#sync1");
     var sync2 = $("#sync2");
+    var sync3 = $("#sync3");
+    var sync4 = $("#sync4");
     var slidesPerPage = 4; //globaly define number of elements per page
     var syncedSecondary = true;
     
@@ -271,12 +273,20 @@
         dots: true,
         loop: true,
         responsiveRefreshRate: 200,
-        navText: [
-            '<div class="custom-nav-btn"><svg width="100%" height="100%" viewBox="0 0 11 20"><path style="fill:none;stroke-width: 1px;stroke: #000;" d="M4.777,0.501l-4.304,4.303l4.304,4.303"/></svg></div>',
-            '<div class="custom-nav-btn"><svg width="100%" height="100%" viewBox="0 0 11 20" version="1.1"><path style="fill:none;stroke-width: 1px;stroke: #000;" d="M0.527,9.107l4.303,-4.303l-4.303,-4.304"/></svg></div>'
-        ],
+        navText: ''
     }).on('changed.owl.carousel', syncPosition);
     
+    sync3.owlCarousel({
+        items: 1,
+        slideSpeed: 2000,
+        nav: true,
+        autoplay: true,
+        dots: true,
+        loop: true,
+        navText: '',
+        responsiveRefreshRate: 200,
+    }).on('changed.owl.carousel', syncPosition3);
+
     sync2
         .on('initialized.owl.carousel', function () {
         sync2.find(".owl-item").eq(0).addClass("current");
@@ -290,6 +300,20 @@
         slideBy: slidesPerPage, //alternatively you can slide by 1, this way the active slide will stick to the first item in the second carousel
         responsiveRefreshRate : 100
     }).on('changed.owl.carousel', syncPosition2);
+
+    sync4
+        .on('initialized.owl.carousel', function () {
+        sync2.find(".owl-item").eq(0).addClass("current");
+        })
+        .owlCarousel({
+        items : slidesPerPage,
+        dots: true,
+        nav: true,
+        smartSpeed: 200,
+        slideSpeed : 500,
+        slideBy: slidesPerPage, //alternatively you can slide by 1, this way the active slide will stick to the first item in the second carousel
+        responsiveRefreshRate : 100
+    }).on('changed.owl.carousel', syncPosition4);
     
     function syncPosition(el) {
         //if you set loop to false, you have to restore this next line
@@ -324,6 +348,40 @@
         sync2.data('owl.carousel').to(current - onscreen, 100, true);
         }
     }
+
+    function syncPosition3(el) {
+        //if you set loop to false, you have to restore this next line
+        //var current = el.item.index;
+        
+        //if you disable loop you have to comment this block
+        var count = el.item.count-1;
+        var current = Math.round(el.item.index - (el.item.count/2) - .5);
+        
+        if(current < 0) {
+        current = count;
+        }
+        if(current > count) {
+        current = 0;
+        }
+        
+        //end block
+    
+        sync4
+        .find(".owl-item")
+        .removeClass("current")
+        .eq(current)
+        .addClass("current");
+        var onscreen = sync4.find('.owl-item.active').length - 1;
+        var start = sync4.find('.owl-item.active').first().index();
+        var end = sync4.find('.owl-item.active').last().index();
+        
+        if (current > end) {
+        sync4.data('owl.carousel').to(current, 100, true);
+        }
+        if (current < start) {
+        sync4.data('owl.carousel').to(current - onscreen, 100, true);
+        }
+    }
     
     function syncPosition2(el) {
         if(syncedSecondary) {
@@ -331,10 +389,11 @@
         sync1.data('owl.carousel').to(number, 100, true);
         }
     }
-    
-    sync2.on("click", ".owl-item", function(e){
-        e.preventDefault();
-        var number = $(this).index();
-        sync1.data('owl.carousel').to(number, 300, true);
-    });
+
+    function syncPosition4(el) {
+        if(syncedSecondary) {
+        var number = el.item.index;
+        sync3.data('owl.carousel').to(number, 100, true);
+        }
+    }
 }(jQuery));
