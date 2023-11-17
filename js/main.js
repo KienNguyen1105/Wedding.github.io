31cb105106e67b3950e2cf1613c166ed0d09a3b6
VanKien <nguyenvankien110597@gmail.com>
2023-11-17 21:14:08 +0700

    dfgdfg

--- js/main.js
+++ js/main.js
@@ -225,20 +225,20 @@
     }
 
     function playAudio(play) {
-        if (audio.paused) {
-            if (bntnmute && bntmute) {
-                bntnmute.style.display = 'block'
-                bntmute.style.display = 'none'
-            }
-            audio.loop = true;
-            audio.play();
-        } else if (!play) {
-            if (bntnmute && bntmute) {
-                bntnmute.style.display = 'none'
-                bntmute.style.display = 'block'
-            }
-            audio.pause();
-        }
+        // if (audio.paused) {
+        //     if (bntnmute && bntmute) {
+        //         bntnmute.style.display = 'block'
+        //         bntmute.style.display = 'none'
+        //     }
+        //     audio.loop = true;
+        //     audio.play();
+        // } else if (!play) {
+        //     if (bntnmute && bntmute) {
+        //         bntnmute.style.display = 'none'
+        //         bntmute.style.display = 'block'
+        //     }
+        //     audio.pause();
+        // }
         isMute = play;
     }
 
@@ -257,9 +257,11 @@
         dots: true,
         loop: true,
         responsiveRefreshRate: 200,
+        animateOut: 'slideOutUp',
+        touchDrag: false,
         navText: [
-            '<div class="custom-nav-btn"><svg width="100%" height="100%" viewBox="0 0 11 20"><path style="fill:none;stroke-width: 1px;stroke: #000;" d="M4.777,0.501l-4.304,4.303l4.304,4.303"/></svg></div>',
-            '<div class="custom-nav-btn"><svg width="100%" height="100%" viewBox="0 0 11 20" version="1.1"><path style="fill:none;stroke-width: 1px;stroke: #000;" d="M0.527,9.107l4.303,-4.303l-4.303,-4.304"/></svg></div>'
+            '<span style="background-color: rgba(31,41,55,.3);border-radius: 9999px;" class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none"><svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 1 1 5l4 4"></path></svg><span class="sr-only">Previous</span></span>',
+            '<span style="background-color: rgba(31,41,55,.3);border-radius: 9999px;" class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none"><svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"></path></svg><span class="sr-only">Next</span></span>'
         ],
     }).on('changed.owl.carousel', syncPosition);
     
